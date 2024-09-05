ac6aa71a4d1c70189472115526b89ab767b62081
letycialinda <167372919+letycialinda@users.noreply.github.com>
2024-09-05 01:25:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const nome = "Letycia"
+const sobrenome = "coelho"
+const idade = "17"
+const altura = "1,55"
+const cidadenatal = "São jose dos pinhais"
+const massa = "57"
+
+
+  console.log(nome)
+console.log(sobrenome)
+console.log(idade)
+console.log(altura)
+console.log(cidadenatal)
+console.log(massa)
